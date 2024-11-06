1d0ab1a51d8eccccaf08025cd11bc0f60d544a85
Canecpro17 <apapapapsapoaopsap@gmail.com>
2024-11-06 21:40:18 +0300

    lab6

--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-
+    lab6 "isuct.ru/informatics2022/laba6"
 	laba4 "isuct.ru/informatics2022/labs/laba4"
 )
 
 func main() {
 	fmt.Println("Луховской Александр Германович")
 	laba4.Laba4run()
+	lab6.Laba6Run()
 }
